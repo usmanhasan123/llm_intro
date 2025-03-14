0eb25a7af03d800cbeb60bf63f16a947c5908702
usmanhasan123 <60095262+usmanhasan123@users.noreply.github.com>
2025-03-14 04:31:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import huggingface_hub
 from langchain.llms import HuggingFaceHub
+import os
+
+os.system("git clone https://github.com/usmanhasan123/llm_intro.git")
 
 # api_key=st.secrets["HUGGINGFACEHUB_API_TOKEN"]
 st.set_page_config(page_title="Langchain demo", page_icon=":robot:")
