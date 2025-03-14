983b9511cad1c2a901673a46b7a97ff20b74c77b
usmanhasan123 <60095262+usmanhasan123@users.noreply.github.com>
2025-03-14 11:16:59 +0000

    added secret

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import streamlit as st
 import huggingface_hub
 from langchain.llms import HuggingFaceHub
 
+api_key=st.secrets["HUGGINGFACEHUB_API_TOKEN"]
 st.set_page_config(page_title="Langchain demo", page_icon=":robot:")
 st.header("Langchain Demo")
-llm=HuggingFaceHub(repo_id="google/gemma-2-2b-it")
+llm=HuggingFaceHub(repo_id="google/gemma-2-2b-it", huggingfacehub_api_token=api_key)
 query=st.text_input("You: ", key="input")
 press=st.button("Generate")
 if press:
