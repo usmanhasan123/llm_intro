ca1b87feadd072d34dbf19d3b3ed4ba40b6bd9e3
usmanhasan123 <60095262+usmanhasan123@users.noreply.github.com>
2025-03-18 14:09:37 -0700

    Update app.py

--- app_text_embedding/app.py
+++ app_text_embedding/app.py
@@ -20,31 +20,35 @@ st.header("Langchain demo for text embeddings")
 embeddings=HuggingFaceHubEmbeddings()
 
 text=st.text_input("You: ", key="input")
-
-sents=nltk.sent_tokenize(text)
-embed_list=[]
-for sent in sents:
-    embed=embeddings.embed_query(sent.lower())
-    embed_list.append(embed)
-    
-sim_list=[]
-for i, sent in enumerate(sents):
-    twod_emed=np.array(embed_list[i]).reshape(1,-1)
-    sim=cosine_similarity(np.array(embed_list[0]).reshape(1,-1), twod_emed)
-    sim_list.append(sim)
-    
-df_sent=pd.DataFrame({"sents": sents, "embeddings": embed_list, "cos_sim": sim_list})
-df_sent["cos_sim_1"]=df_sent.apply(lambda x: x["cos_sim"][0][0], axis=1)
-df_sent=df_sent.sort_values(by="cos_sim_1", ascending=False)
-summary=""
-
 words=st.text_input("summary words: ", key="input")
 
-for i in df_sent["sents"][:words]:
-    summary=summary+i
-    summary=summary+" "
-    
 press=st.button("Generate summary")
 if press:
-    st.subheader("Summary")
-    st.write(summary)
+    if text=="":
+        st.write("You haven't provided any text yet")
+    else:
+        sents=nltk.sent_tokenize(text)
+        embed_list=[]
+        for sent in sents:
+            embed=embeddings.embed_query(sent.lower())
+            embed_list.append(embed)
+            
+        sim_list=[]
+        for i, sent in enumerate(sents):
+            twod_emed=np.array(embed_list[i]).reshape(1,-1)
+            sim=cosine_similarity(np.array(embed_list[0]).reshape(1,-1), twod_emed)
+            sim_list.append(sim)
+            
+        df_sent=pd.DataFrame({"sents": sents, "embeddings": embed_list, "cos_sim": sim_list})
+        df_sent["cos_sim_1"]=df_sent.apply(lambda x: x["cos_sim"][0][0], axis=1)
+        df_sent=df_sent.sort_values(by="cos_sim_1", ascending=False)
+        summary=""
+        
+        for i in df_sent["sents"][:words]:
+            summary=summary+i
+            summary=summary+" "
+    
+        st.subheader("Summary")
+        st.write(summary)
+            
+        
