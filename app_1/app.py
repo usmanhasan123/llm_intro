f88af9b8d04f50f6b498c1ffea1941af3452ee85
usmanhasan123 <60095262+usmanhasan123@users.noreply.github.com>
2025-07-21 04:54:15 -0700

    Update app.py

--- app_1/app.py
+++ app_1/app.py
@@ -9,7 +9,7 @@ import os
 api_key=os.getenv("HUGGINGFACEHUB_API_TOKEN")
 st.set_page_config(page_title="Langchain demo", page_icon=":robot:")
 st.header("Langchain Demo")
-llm=HuggingFaceHub(repo_id="google/gemma-2-2b-it")
+llm=HuggingFaceHub(repo_id="google/gemma-2-2b-it", huggingfacehub_api_token=api_key)
 query=st.text_input("You: ", key="input")
 press=st.button("Generate")
 if press:
